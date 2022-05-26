80fde69279d90cf0e3041cc1e42e0de77b7f1d9b
czm <yogkin@163.com>
2022-05-26 13:43:36 +0800

    更新demo 适配nullable 特性

--- lib/base/net/my_http_interceptor.dart
+++ lib/base/net/my_http_interceptor.dart
@@ -4,19 +4,9 @@ import 'package:my_net_lib/my_net_lib.dart';
 
 class MYInterceptor extends InterceptorsWrapper {
   @override
-  Future onRequest(RequestOptions options) {
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
     options.headers ??= <String, dynamic>{};
     options.headers.addAll({'version': '6350', ...Aes.getHeaderParams()});
-    return super.onRequest(options);
-  }
-
-  @override
-  Future onResponse(Response response) {
-    return super.onResponse(response);
-  }
-
-  @override
-  Future onError(DioError err) {
-    return super.onError(err);
+    super.onRequest(options, handler);
   }
 }
